8428a77a3b200d476b4c4e370d07f7e96f75b355
Ben Moss <ben@mossity.com>
2022-11-10 15:02:54 -0500

    automatic retries?? (#4)
    * automatic retries??
    
    * run_id instead of ref

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -eu
 
-yq eval '.steps[].agents.queue += env(GITHUB_REF)' "$(dirname "$0")/pipeline.yaml" | buildkite-agent pipeline upload
+yq eval '.steps[].agents.queue += env(GITHUB_RUN_ID)' "$(dirname "$0")/pipeline.yaml" | buildkite-agent pipeline upload
